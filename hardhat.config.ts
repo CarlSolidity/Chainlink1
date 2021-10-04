12e33b633bc3912522591a203834575b12402cb7
CarlSolidity <carl.charpin@protonmail.com>
2021-10-03 22:46:21 -0400

    Modified import contract solidity version, modified config to add forking network for test, created test from tutorial

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -18,5 +18,19 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [{ version: "0.8.7", settings: {} }],
   },
+  defaultNetwork: 'hardhat',
+  networks: {
+    hardhat: {
+      forking: {
+        url: process.env.ALCHEMY_MAINNET_RPC_URL // ADD URL FROM ALCHEMY/INFURA
+      }
+    },
+    ropsten: {
+      url: process.env.ROPSTEN_RPC_URL, //add url to env
+      accounts: {
+        mnemonic: process.env.MNEMONIC //Add mnemonic to environment TEST ONLY
+      }
+    }
+  }
 };
 export default config;
